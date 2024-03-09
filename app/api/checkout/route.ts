ae75b948846772ce763f167b9494046186da8c04
wphoenix <phoenix.wan.china@gmail.com>
2024-03-09 16:47:32 +0800

    修改超时时间

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -6,7 +6,7 @@ import Stripe from "stripe";
 import { currentUser } from "@clerk/nextjs";
 import { genOrderNo } from "@/lib/order";
 
-export const maxDuration = 90;
+export const maxDuration = 9;
 
 export async function POST(req: Request) {
   const user = await currentUser();
